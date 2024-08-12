bce7f902cfecc95377cd48725927f2db137ad865
Ben Graham <grahamben7@gmail.com>
2024-08-12 21:01:02 +0200

    Adding 4 columns to AddInspectionRecord

--- app/pages/AddRecord/page.jsx
+++ app/pages/AddRecord/page.jsx
@@ -49,6 +49,52 @@ export default function page() {
 						</div>
     
 					</div>
+
+					<div id="Right-Side" className='flex flex-col space-y-4'>
+						<div>
+							<label class="block text-black text-lg mb-1" for="project">
+								Project
+							</label>
+							<input id="project" placeholder="Project" class="shadow appearance-none border border-blue-600 bg-inherit rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"  type="text" />
+						</div>
+						<div>
+							<label class="block text-black text-lg mb-1" for="GRV">
+								GRV
+							</label>
+							<input id="GRV" placeholder="GRV" class="shadow appearance-none border border-blue-600 bg-inherit rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"  type="date"/>
+						</div>
+
+						<div>
+							<label class="block text-black text-lg mb-1" for="date-inspected">
+								Date Inspected
+							</label>
+							<input id="date-inspected" placeholder="Date Inspected" class="shadow appearance-none border border-blue-600 bg-inherit rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"  type="Date"/>
+						</div>
+    
+					</div>
+					<div id="Right-Side" className='flex flex-col space-y-4'>
+						<div>
+							<label class="block text-black text-lg mb-1" for="project">
+								Project
+							</label>
+							<input id="project" placeholder="Project" class="shadow appearance-none border border-blue-600 bg-inherit rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"  type="text" />
+						</div>
+						<div>
+							<label class="block text-black text-lg mb-1" for="GRV">
+								GRV
+							</label>
+							<input id="GRV" placeholder="GRV" class="shadow appearance-none border border-blue-600 bg-inherit rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"  type="date"/>
+						</div>
+
+						<div>
+							<label class="block text-black text-lg mb-1" for="date-inspected">
+								Date Inspected
+							</label>
+							<input id="date-inspected" placeholder="Date Inspected" class="shadow appearance-none border border-blue-600 bg-inherit rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"  type="Date"/>
+						</div>
+    
+					</div>
+
 				</div>
 
 
@@ -56,7 +102,7 @@ export default function page() {
 
 			<section className='flex justify-center space-x-9'>
 				<Link href={"/"} className='px-4 py-2 bg-red-600 text-white rounded'>Cancel</Link>
-				<button className='px-4 py-2 bg-blue-600 text-white rounded'>Add Inspection Record</button>
+				<button className='px-4 py-2 bg-blue-600 text-white rounded'>Complete</button>
 			</section>
 		</main>
     )
