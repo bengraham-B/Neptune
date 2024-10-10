9a4c869328de0a1623d522945e8bd28ca2bf94ff
bengraham-B <grahamben7@gmail.com>
2024-10-10 06:38:31 +0200

    Before pull

--- app/database/database.js
+++ app/database/database.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
     host: "localhost",
     user: process.env.DATABASE_USERNAME,
-    port: process.env.DATABASE_PORT, 
+    // port: process.env.DATABASE_PORT, 
     host: process.env.DATABASE_HOST,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_DATABASE
